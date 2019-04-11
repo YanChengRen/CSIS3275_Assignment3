a8cc181462dbe368b9c1f3b2feb271950c340256
YanChengRen <47011989+YanChengRen@users.noreply.github.com>
2019-04-10 21:37:22 -0700

    Added my name to HelloWorld
    Added YanCheng Ren to HelloWorld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,12 +3,14 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
 		System.out.print("hello word");
-=======
 		
-		//test
->>>>>>> df3a036345ee3023600a76d63670a5786282c48e
+		/* Name: Yan Cheng Ren
+		 Student ID: 300179993
+		*/
+		
+		
+	
 
 	}
 
