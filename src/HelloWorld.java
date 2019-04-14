d31fd3a7302a43d3bf259bf40684e5cc978441c9
YanChengRen <47011989+YanChengRen@users.noreply.github.com>
2019-04-13 22:36:23 -0700

    test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,15 +4,13 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("hello word");
-<<<<<<< HEAD
          // student id:300293244
          // sumit singh
-=======
-=======
->>>>>>> a8cc181462dbe368b9c1f3b2feb271950c340256
+
 		
 		/* Name: Yan Cheng Ren
 		 Student ID: 300179993
+		 test
 		*/
 		
 		
